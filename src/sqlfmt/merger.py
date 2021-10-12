56822d1e2900246c29485257333c7e1dae18b120
tconbeer <tconbeer@makespace.com>
2021-10-11 19:56:10 -0600

    refactor merger

--- src/sqlfmt/merger.py
+++ src/sqlfmt/merger.py
@@ -6,9 +6,57 @@ from sqlfmt.mode import Mode
 from sqlfmt.token import TokenType
 
 
+class CannotMergeException(Exception):
+    pass
+
+
 @dataclass
 class LineMerger:
     mode: Mode
+
+    @classmethod
+    def create_merged_line(
+        cls,
+        lines: List[Line],
+        parent_idx: int,
+        child_idx: int,
+    ) -> Line:
+        """
+        Returns a new line by merging together all nodes in the slice of lines
+        from parent_idx to child_idx. Raises an exception if attempting to
+        merge one line, or if the returned line would be empty or would attempt
+        to merge a multiline token.
+        """
+
+        # if the child is just one below the parent, we're trying to
+        # merge a single line.
+        if parent_idx - child_idx == 1:
+            raise CannotMergeException("Can't merge just one line")
+
+        parent_line = lines[parent_idx]
+        merged_nodes: List[Node] = []
+
+        for line in lines[parent_idx:child_idx]:
+            # skip over nodes containing NEWLINEs
+            nodes = [
+                node for node in line.nodes if node.token.type != TokenType.NEWLINE
+            ]
+            merged_nodes.extend(nodes)
+
+        if not merged_nodes:
+            raise CannotMergeException("Can't merge only whitespace/newlines")
+        elif any([n.is_multiline for n in merged_nodes]):
+            raise CannotMergeException("Can't merge lines containing multiline nodes")
+
+        merged_line = Line.from_nodes(
+            source_string=parent_line.source_string,
+            previous_node=parent_line.previous_node,
+            nodes=merged_nodes,
+        )
+
+        merged_line.append_newline()
+
+        return merged_line
 
     def maybe_merge_lines(self, lines: List[Line], from_depth: int = 0) -> List[Line]:
         """
@@ -19,7 +67,6 @@ class LineMerger:
         is reached again. We do this recursively, by calling this
         method again, with a copied slice of lines
         """
-        MAX_LENGTH = self.mode.line_length
         if len(lines) == 1:
             return lines
 
@@ -32,8 +79,9 @@ class LineMerger:
                 break
             if parent_line.change_in_depth > 0:
 
-                # scan ahead until we get back to this depth, or hit EOF
-                # note that we're using the same generator as parent loop
+                # scan ahead until we get back to this depth, or hit EOF.
+                # note that we're using the same generator as parent loop.
+                # initialize child values in case we've exhausted scanner
                 child_line: Optional[Line]
                 child_idx = parent_idx
                 child_depth = parent_depth
@@ -80,35 +128,13 @@ class LineMerger:
                 ):
                     child_idx += 1
 
-                # if the child is just one below the parent, we're trying to
-                # merge a single line. break to return.
-                if parent_idx - child_idx == 1:
+                # Now we merge the slice from parent_idx:child_idx
+                try:
+                    merged_line = self.create_merged_line(lines, parent_idx, child_idx)
+                except CannotMergeException:
                     break
 
-                # Now we merge the slice from parent_idx:child_idx
-                source_string = parent_line.source_string
-                merged_nodes: List[Node] = []
-                for line in lines[parent_idx:child_idx]:
-                    # skip over nodes containing NEWLINEs
-                    nodes = [
-                        node
-                        for node in line.nodes
-                        if node.token.type != TokenType.NEWLINE
-                    ]
-                    merged_nodes.extend(nodes)
-                if not merged_nodes:
-                    # we only have whitespace/newlines
-                    break
-                elif any([n.is_multiline for n in merged_nodes]):
-                    # there's a multiline node in there. abort!
-                    break
-                merged_line = Line.from_nodes(
-                    source_string=source_string,
-                    previous_node=parent_line.previous_node,
-                    nodes=merged_nodes,
-                )
-                merged_line.append_newline()
-                if len(merged_line) <= MAX_LENGTH:
+                if len(merged_line) <= self.mode.line_length:
                     lines[parent_idx:child_idx] = [merged_line]
                     # continuing to iterate over the same scanner won't work, since
                     # the indexes have changed. Recurse to merge the tail
