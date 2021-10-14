5616925635f7d0609d4666617bd1c529799e2a18
tconbeer <tconbeer@makespace.com>
2021-10-14 08:44:55 -0600

    refactor cli tests

---
+++ tests/functional_tests/test_end_to_end.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import Any
+
+import pytest
+from click.testing import CliRunner
+
+from sqlfmt.cli import sqlfmt as sqlfmt_main
+from tests.test_utils import copy_test_data_to_tmp
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    return CliRunner(mix_stderr=False)
+
+
+@pytest.fixture(
+    params=[
+        [
+            "basic_queries/001_select_1.sql",
+            "basic_queries/002_select_from_where.sql",
+        ],
+        ["basic_queries"],
+    ]
+)
+def target_directory(request: Any, tmp_path: Path) -> Path:
+    """
+    Copies the parameterized list of files/directories from the test/data
+    directory into a temp directory (provided by pytest fixture tmp_path),
+    and then returns the path to the temp directory.
+    """
+    test_dir = copy_test_data_to_tmp(request.param, tmp_path)
+    return test_dir
+
+
+@pytest.mark.parametrize("options", ["", "--line-length 88", "-l 88"])
+def test_end_to_end(runner: CliRunner, target_directory: Path, options: str) -> None:
+
+    args = f"{target_directory} {options}"
+    result = runner.invoke(sqlfmt_main, args=args)
+
+    assert result
+    assert result.exit_code == 0
--- tests/unit_tests/test_cli.py
+++ tests/unit_tests/test_cli.py
@@ -22,12 +22,13 @@ def test_click_cli_runner_is_equivalent_to_py_subprocess() -> None:
     click_results = CliRunner().invoke(sqlfmt_main)
 
     assert builtin_results.returncode == click_results.exit_code
-    assert builtin_results.stdout == click_results.output
+    assert builtin_results.stdout == click_results.stdout
 
 
 def test_help_command() -> None:
     # Sally installs sqlfmt; not knowing where to start, she types "sqlfmt --help" into
     # her command line, and sees that it displays the version and a help menu
-    help_command = ["sqlfmt", "--help"]
-    help_results = run_cli_command(help_command)
-    assert help_results.returncode == 0
+    help_option = "--help"
+    help_results = CliRunner().invoke(sqlfmt_main, args=help_option)
+    assert help_results.exit_code == 0
+    assert help_results.stdout.startswith("Usage: sqlfmt")
