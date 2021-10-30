7f2212f08b5da5a4da3b87cb8c2d6f94238ebf30
Ted Conbeer <tconbeer@users.noreply.github.com>
2021-10-30 12:34:16 -0600

    feature/sc 78/add ci cd using tox gh actions (#19)
    * add tox config, support py37
    
    * add gh workflow
    
    * fix workflow-on
    
    * Use tox4 for Fox-gh
    
    * fix py matrix, remove windows
    
    * run linters inside poetry env

--- src/sqlfmt/__init__.py
+++ src/sqlfmt/__init__.py
@@ -1,5 +1,11 @@
-from importlib.metadata import version
+import sys
 from pathlib import Path
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+
 
 __version__ = version("sqlfmt")
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
