662a7f7a6df285261b68fd735a3006603eeb2df2
tconbeer <tconbeer@makespace.com>
2021-11-19 09:36:31 -0700

    refactor: performance enhancements for lexing

--- src/sqlfmt/parser.py
+++ src/sqlfmt/parser.py
@@ -34,66 +34,69 @@ class Query:
         q = Query(source_string, mode)
         line = Line(source_string=source_string, previous_node=None)
         q.lines.append(line)
-        for token in q.lex(tail=q.source_string, pos=0):
+        for token in q.lex(source_string=source_string, pos=0):
             line.append_token(token)
         return q
 
-    def lex(self, tail: str, pos: int) -> Iterable[Token]:
+    def lex(self, source_string: str, pos: int) -> Iterable[Token]:
         """
         Generate tokens by matching the head of the passed string,
-        and then recursing on the tail
+        and then moving the pointer to the head and repeating on
+        the rest of the string
         """
-        while True:
-            if tail.strip() == "":
-                return
-            else:
-                for token_type, prog in self.programs.items():
+        eof_pos = -1
+        for idx, char in enumerate(reversed(source_string)):
+            if not char.isspace():
+                eof_pos = len(source_string) - idx
+                break
 
-                    match = prog.match(tail)
-                    if match:
-                        spos = pos
-                        start, end = match.span(1)
-                        prefix = tail[:start]
-                        raw_token = tail[start:end]
-                        if token_type in (TokenType.JINJA_END, TokenType.COMMENT_END):
-                            raise SqlfmtMultilineError(
-                                f"Encountered closing bracket '{raw_token}' at position"
-                                f" {pos+start}, before matching opening bracket:"
-                                f" {tail[start:start+50]}"
-                            )
-                        elif token_type in (
-                            TokenType.JINJA_START,
-                            TokenType.COMMENT_START,
-                        ):
-                            # search for the ending token, and/or nest levels deeper
-                            epos = self.search_for_terminating_token(
-                                start_type=token_type,
-                                tail=tail[end:],
-                                pos=pos + end,
-                            )
-                            end = epos - pos
-                            token = tail[start:end]
-                            if token_type == TokenType.JINJA_START:
-                                token_type = TokenType.JINJA
-                            else:
-                                token_type = TokenType.COMMENT
+        while pos < eof_pos:
+
+            for token_type, prog in self.programs.items():
+
+                match = prog.match(source_string, pos)
+                if match:
+                    spos, epos = match.span(1)
+                    prefix = source_string[pos:spos]
+                    raw_token = source_string[spos:epos]
+                    if token_type in (TokenType.JINJA_END, TokenType.COMMENT_END):
+                        raise SqlfmtMultilineError(
+                            f"Encountered closing bracket '{raw_token}' at position"
+                            f" {spos}, before matching opening bracket:"
+                            f" {source_string[spos:spos+50]}"
+                        )
+                    elif token_type in (
+                        TokenType.JINJA_START,
+                        TokenType.COMMENT_START,
+                    ):
+                        # search for the ending token, and/or nest levels deeper
+                        epos = self.search_for_terminating_token(
+                            start_type=token_type,
+                            tail=source_string[epos:],
+                            pos=epos,
+                        )
+                        token = source_string[spos:epos]
+                        if token_type == TokenType.JINJA_START:
+                            token_type = TokenType.JINJA
                         else:
-                            epos = pos + end
-                            token = raw_token
+                            token_type = TokenType.COMMENT
+                    else:
+                        token = raw_token
 
-                        assert end > 0
+                    assert (
+                        epos > 0
+                    ), "Internal Error! Something went wrong with jinja parsing"
 
-                        yield Token(token_type, prefix, token, spos, epos)
+                    yield Token(token_type, prefix, token, pos, epos)
 
-                        tail = tail[end:]
-                        pos = epos
-                        break
-                # nothing matched. Either whitespace or an error
-                else:
-                    raise SqlfmtParsingError(
-                        f"Could not parse SQL at position {pos}:"
-                        f" '{tail[pos:pos+50].strip()}'"
-                    )
+                    pos = epos
+                    break
+            # nothing matched. Either whitespace or an error
+            else:
+                raise SqlfmtParsingError(
+                    f"Could not parse SQL at position {pos}:"
+                    f" '{source_string[pos:pos+50].strip()}'"
+                )
 
     def search_for_terminating_token(
         self, start_type: TokenType, tail: str, pos: int
