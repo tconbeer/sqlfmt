d8829fa8cc5debca7d3770cfc91179737df16a61
Ted Conbeer <tconbeer@users.noreply.github.com>
2022-12-05 16:19:18 +0000

    fix: add backport for py 3.7

--- src/sqlfmt/token.py
+++ src/sqlfmt/token.py
@@ -1,7 +1,12 @@
 import re
+import sys
 from enum import Enum, auto
-from functools import cached_property
 from typing import NamedTuple
+
+if sys.version_info >= (3, 8):
+    from functools import cached_property
+else:
+    from backports.cached_property import cached_property
 
 
 class TokenType(Enum):
