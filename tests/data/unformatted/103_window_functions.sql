cd38a6c35dc5c06a32517f68c03a5e8e3aa7f8fe
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-04-13 06:26:11 -0600

    fix: support frame clauses using range, groups

--- tests/data/unformatted/103_window_functions.sql
+++ tests/data/unformatted/103_window_functions.sql
@@ -4,7 +4,9 @@ select
     row_number() over () as c,
     count(case when a is null then 1 end) over (partition by user_id, date_trunc('year', performed_at)) as d,
     first_value(a ignore nulls) over (partition by user_id order by performed_at desc rows between unbounded preceding and unbounded following) as e,
-    count(*) filter (WHERE a is null) over (partition by user_id, date_trunc('year', performed_at)) as f
+    count(*) filter (WHERE a is null) over (partition by user_id, date_trunc('year', performed_at)) as f,
+    first_value(a ignore nulls) over (partition by user_id order by performed_at desc range between unbounded preceding and unbounded following) as g,
+    last_value(a ignore nulls) over (partition by user_id order by performed_at asc rows 5 preceding exclude current row) as h
 from
     my_table
 )))))__SQLFMT_OUTPUT__(((((
@@ -22,5 +24,15 @@ select
     ) as e,
     count(*) filter (where a is null) over (
         partition by user_id, date_trunc('year', performed_at)
-    ) as f
+    ) as f,
+    first_value(a ignore nulls) over (
+        partition by user_id
+        order by performed_at desc
+        range between unbounded preceding and unbounded following
+    ) as g,
+    last_value(a ignore nulls) over (
+        partition by user_id
+        order by performed_at asc
+        rows 5 preceding exclude current row
+    ) as h
 from my_table
