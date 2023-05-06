d84ff5bbd34b8b78da75270b229c20dd71179b30
Ted Conbeer <tconbeer@users.noreply.github.com>
2023-05-06 05:14:01 -0600

    chore: bump primer refs

--- src/sqlfmt_primer/primer.py
+++ src/sqlfmt_primer/primer.py
@@ -75,7 +75,7 @@ def get_projects() -> List[SQLProject]:
         SQLProject(
             name="dbt_utils",
             git_url="https://github.com/tconbeer/dbt-utils.git",
-            git_ref="ae160c9",  # sqlfmt 54b8edd
+            git_ref="c62b99f",  # sqlfmt 6e9615c
             expected_changed=0,
             expected_unchanged=131,
             expected_errored=0,
