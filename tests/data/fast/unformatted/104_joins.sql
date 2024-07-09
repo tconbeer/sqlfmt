835726a9a8191b102b7bc3ad1c1265be4baab2ca
Colin <23076617+clenost@users.noreply.github.com>
2024-07-09 17:54:27 +0200

    feat(join-lint): handle left anti join

--- tests/data/fast/unformatted/104_joins.sql
+++ tests/data/fast/unformatted/104_joins.sql
@@ -1,5 +1,5 @@
 select one.one, two.two, 
-three.three, four.four, five.five, six.six
+three.three, four.four, five.five, six.six, seven.seven
 from one
 join two on one.two = two.one
 inner join "my_database"."my_schema".three as three on one.three = three.one
@@ -12,9 +12,10 @@ right join (
     from my_table where some_filter is true
 ) as five using(five.id)
 natural full outer join six
+left anti join seven on one.seven = seven.one
 cross join {{ ref('bar_bar_bar') }} as bar
 )))))__SQLFMT_OUTPUT__(((((
-select one.one, two.two, three.three, four.four, five.five, six.six
+select one.one, two.two, three.three, four.four, five.five, six.six, seven.seven
 from one
 join two on one.two = two.one
 inner join "my_database"."my_schema".three as three on one.three = three.one
@@ -29,4 +30,5 @@ right join
         select id, five, six, seven, eight, nine from my_table where some_filter is true
     ) as five using (five.id)
 natural full outer join six
+left anti join seven on one.seven = seven.one
 cross join {{ ref("bar_bar_bar") }} as bar
