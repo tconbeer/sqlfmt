dc00efa6dbb672ce5174d7907460697da5c84eb7
Ted Conbeer <tconbeer@users.noreply.github.com>
2025-01-24 15:13:29 -0700

    fix #637: no space between a colon and square bracket

---
+++ tests/data/unformatted/136_databricks_variant.sql
@@ -0,0 +1,6 @@
+-- https://github.com/tconbeer/sqlfmt/issues/637
+SELECT e.col:['attr_ID']::string as id
+FROM expl e
+)))))__SQLFMT_OUTPUT__(((((
+-- https://github.com/tconbeer/sqlfmt/issues/637
+select e.col:['attr_ID']::string as id from expl e
