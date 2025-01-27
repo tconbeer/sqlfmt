436c39ea81dc7090563b44859d1412690bddbb73
Ted Conbeer <tconbeer@users.noreply.github.com>
2025-01-27 15:28:10 -0700

    fix: lex quoted names as data in unsupported ddl blocks (#667)

--- src/sqlfmt/rules/unsupported.py
+++ src/sqlfmt/rules/unsupported.py
@@ -2,12 +2,22 @@ from functools import partial
 
 from sqlfmt import actions
 from sqlfmt.rule import Rule
-from sqlfmt.rules.common import NEWLINE, group
+from sqlfmt.rules.common import NEWLINE, SQL_QUOTED_EXP, group
 from sqlfmt.rules.core import ALWAYS
 from sqlfmt.tokens import TokenType
 
 UNSUPPORTED = [
     *ALWAYS,
+    # quoted names need to be lexed as DATA, so they are not formatted.
+    # quoted names are otherwise in ALWAYS, (so we don't lex them as comments etc.)
+    # so this rule needs to have a higher priority than the quoted_name rule in
+    # ALWAYS
+    Rule(
+        name="quoted_name_in_unsupported",
+        priority=199,
+        pattern=SQL_QUOTED_EXP,
+        action=partial(actions.add_node_to_buffer, token_type=TokenType.DATA),
+    ),
     Rule(
         name="unsupported_line",
         priority=1000,
--- tests/data/unformatted/999_unsupported_ddl.sql
+++ tests/data/unformatted/999_unsupported_ddl.sql
@@ -2,6 +2,11 @@ CREATE PUBLICATION insert_only FOR TABLE mydata
     WITH (publish = 'insert');
 CREATE PUBLICATION production_publication FOR TABLE users, departments, TABLES IN SCHEMA production;
 CREATE PUBLICATION users_filtered FOR TABLE users (user_id, firstname);
+create table foo as (
+    aaa text,
+    "bBb" int,
+    ccc date
+);
 SELECT
     1;
 )))))__SQLFMT_OUTPUT__(((((
@@ -9,5 +14,10 @@ CREATE PUBLICATION insert_only FOR TABLE mydata
     WITH (publish = 'insert');
 CREATE PUBLICATION production_publication FOR TABLE users, departments, TABLES IN SCHEMA production;
 CREATE PUBLICATION users_filtered FOR TABLE users (user_id, firstname);
+create table foo as (
+    aaa text,
+    "bBb" int,
+    ccc date
+);
 select 1
 ;
