22fa5878a205f8865ef8a9fc61a13a90e1b6a7c5
Victor Solano <victor.solanonunez@gmail.com>
2026-08-10 10:03:23 +0200

    fix: handle digit-started identifiers without underscores

--- tests/data/unformatted/110_other_identifiers.sql
+++ tests/data/unformatted/110_other_identifiers.sql
@@ -11,14 +11,13 @@ select
 -- SparkSQL permits field names that start with a digit
 select
     substring(f.9021_web_flag, 1, 11),
+    f.9021webflag,
     f.1st_visit,
     9021_web_flag,
     f.1_000_flag
   from f
--- a number abutting a keyword must still split; these guard the lookahead
--- against being widened to (?!\w), which would lex 1as and 1and as one name
+-- Spark regular identifiers do not require an underscore
 select 1as x from t
-select 1 from t where x=1and y=2
 select 1_000 + 2_000 from t
 )))))__SQLFMT_OUTPUT__(((((
 select v.$1, v.$2, ?3, ?4
@@ -27,14 +26,15 @@ select metadata$filename as file_name, metadata$file_last_modified as file_last_
 from @my_stage(file_format => 'csv_format', pattern => '.*my_pattern.*')
 -- see: https://github.com/tconbeer/sqlfmt/issues/697
 -- SparkSQL permits field names that start with a digit
-select substring(f.9021_web_flag, 1, 11), f.1st_visit, 9021_web_flag, f.1_000_flag
+select
+    substring(f.9021_web_flag, 1, 11),
+    f.9021webflag,
+    f.1st_visit,
+    9021_web_flag,
+    f.1_000_flag
 from f
--- a number abutting a keyword must still split; these guard the lookahead
--- against being widened to (?!\w), which would lex 1as and 1and as one name
-select 1 as x
+-- Spark regular identifiers do not require an underscore
+select 1as x
 from t
-select 1
-from t
-where x = 1 and y = 2
 select 1_000 + 2_000
 from t
